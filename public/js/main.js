44e07f3669d7be1216100d8c4a06c7c93dcfe3a6
hfdhakbar <mhakbar1234@gmail.com>
2025-07-15 23:53:55 +0700

    feat:Starting add JS

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+function toggleOverlay(card) {
+  const overlay = card.querySelector('div:nth-child(2)');
+  overlay.classList.toggle('opacity-0');
+  overlay.classList.toggle('pointer-events-none');
+  overlay.classList.toggle('opacity-100');
+  overlay.classList.toggle('pointer-events-auto');
+}
